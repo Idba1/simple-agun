2c404a89ab98b0adc60d1be701c5b0798a0fd47f
Idba1 <moniraislamself@gmail.com>
2024-04-04 03:28:11 +0600

    show details in display from user email

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,24 +1,33 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 const Login = () => {
+    const [saveUser, setSaveUser] = useState([])
     const auth = getAuth(app);
-    console.log(auth);
+    // console.log(auth);
     const provider = new GoogleAuthProvider();
 
-    const handleGoogleSignIn = ()=>{
+    const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
-        .then((result)=> {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch((error)=>{
-            console.log('error', error.message);
-        })
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+                setSaveUser(user)
+            })
+            .catch((error) => {
+                console.log('error', error.message);
+            })
     }
 
     return (
         <div>
             <button onClick={handleGoogleSignIn}>Login</button>
+            {
+                saveUser && <div>
+                    <h2>User: {saveUser.displayName}</h2>
+                    <img src={saveUser.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
